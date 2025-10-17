7824abf83b4f0a15ed117e79cb6121e57dc5cef2
Yogesh Prajapati <verma981133@gmail.com>
2025-10-17 15:54:38 +0530

    fix: Fixing Color and Typography Scheme

--- app/src/main/java/com/yogesh/stylish/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/yogesh/stylish/presentation/ui/theme/Color.kt
@@ -4,6 +4,8 @@ import androidx.compose.ui.graphics.Color
 
 // FIGMA DESIGN COLORS WITH HEX CODE
 
+/* NOW IT IS EASIER TO FOLLOW FIGMA DESIGN IN THE APP */
+
 // --- BRAND COLORS ---
 val PrimaryRed = Color(0xFFF83758)
 val PrimaryBlack = Color(0xFF000000)
