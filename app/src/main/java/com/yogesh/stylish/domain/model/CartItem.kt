fa6c4567f92c88376ba0f29050a7bf73cb65a176
Yogesh Verma <163139864+YogeshPrajapatii@users.noreply.github.com>
2026-05-15 20:37:53 +0530

    Update CartItem.kt

--- app/src/main/java/com/yogesh/stylish/domain/model/CartItem.kt
+++ app/src/main/java/com/yogesh/stylish/domain/model/CartItem.kt
@@ -2,6 +2,7 @@ package com.yogesh.stylish.domain.model
 
 import kotlinx.serialization.Serializable
 
+
 @Serializable
 data class CartItem(val product: Product, val quantity: Int = 1, val selectedSize: String
 )
