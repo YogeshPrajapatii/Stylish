d7bb15ed8e4e54414a44bc8a1e8a20ccded53d87
Yogesh Verma <163139864+YogeshPrajapatii@users.noreply.github.com>
2026-05-15 20:39:31 +0530

    Clean up CartItem.kt by removing blank lines
    Removed unnecessary blank lines in CartItem.kt

--- app/src/main/java/com/yogesh/stylish/domain/model/CartItem.kt
+++ app/src/main/java/com/yogesh/stylish/domain/model/CartItem.kt
@@ -1,8 +1,6 @@
 package com.yogesh.stylish.domain.model
 
 import kotlinx.serialization.Serializable
-
-
 @Serializable
 data class CartItem(val product: Product, val quantity: Int = 1, val selectedSize: String
 )
