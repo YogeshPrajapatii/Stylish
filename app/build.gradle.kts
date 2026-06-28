e7ce2a4e84262a951979dee26967cffe99ddb7fe
Yogesh Verma <163139864+YogeshPrajapatii@users.noreply.github.com>
2026-06-28 22:41:48 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
    alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.google.gms.google.services)
+   alias(libs.plugins.google.gms.google.services)
     alias(libs.plugins.google.dagger.hilt.android)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.compose)
